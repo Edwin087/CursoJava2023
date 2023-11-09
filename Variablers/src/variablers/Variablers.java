7e5a05a6440bc287810158702df8387b351e5d0d
Edwin Calle <calleojeda4@gmail.com>
2023-11-09 09:35:51 -0500

    variables

---
+++ Variablers/src/variablers/Variablers.java
@@ -0,0 +1,24 @@
+
+public class Variablers {
+
+    public static void main(String[] args) {
+    
+        int miVaribleEntera =10;
+        System.out.println(miVaribleEntera);
+        //modificamos el valor de la variable
+        miVaribleEntera = 5;
+        System.out.println(miVaribleEntera);
+        String miVariableCadena="Edwin";
+        System.out.println(miVariableCadena);
+        miVariableCadena="Geovanny";
+        System.out.println(miVariableCadena);
+        //utilización de var interferencia de tipos de java
+        var miVariableEntera2= 15;
+        System.out.println(miVariableEntera2);
+      var miVariavleCadena2="calle";
+        System.out.println("miVariavleCadena2 = " + miVariavleCadena2);
+        
+        
+    }
+    
+}
